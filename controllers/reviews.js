04d789e4d8c398d270402c6fdbeb903d8636de5d
Gudkova Mariia <gudmary8@gmail.com>
2023-02-11 13:47:39 +0400

    add new tables MySQL

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -50,10 +50,10 @@ const getAllUserReviews = async (req, res, next) => {
 const createReview = async (req, res, next) => {
   try {
     const {
-      title, workTitle, description, cover = 'https://images.unsplash.com/photo-1614850715649-1d0106293bd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', coverTwo = 'https://images.unsplash.com/photo-1614850716626-873413eb7c1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', createDate, rating, category, averageReviewRating = '0', id,
+      title, workTitle, description, cover = 'https://images.unsplash.com/photo-1614850715649-1d0106293bd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', coverTwo = 'https://images.unsplash.com/photo-1614850716626-873413eb7c1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', rating, userId, categoryId,
     } = req.body;
     const newReview = await new Promise((resolve, reject) => {
-      db.query('INSERT INTO reviews (title, workTitle, description, cover, coverTwo, createDate, rating, category, averageReviewRating, owner) VALUES(?,?,?,?,?,?,?,?,?,?)', [title, workTitle, description, cover, coverTwo, createDate, rating, category, averageReviewRating, id], (err, result) => {
+      db.query('INSERT INTO reviews (createDate, title, workTitle, description, cover, coverTwo, rating, user_id, category_id) VALUES(now(),?,?,?,?,?,?,?, ?)', [title, workTitle, description, cover, coverTwo, rating, userId, categoryId], (err, result) => {
         if (err) {
           reject(err);
           return;
--- utils/createDB.js
+++ utils/createDB.js
@@ -7,12 +7,32 @@ db.query('CREATE DATABASE reviewsApi', (err) => {
 });
 
 // Create Tables
-db.query('CREATE TABLE users( id INT PRIMARY KEY AUTO_INCREMENT, email VARCHAR(60) NOT NULL UNIQUE, name VARCHAR(30) NOT NULL, password VARCHAR(100) NOT NULL, avatar BLOB, allReviews TINYINT, allLikes TINYINT, allComments TINYINT)', (err) => {
+db.query('CREATE TABLE users( id INT PRIMARY KEY AUTO_INCREMENT, email VARCHAR(60) NOT NULL UNIQUE, name VARCHAR(30) NOT NULL, password VARCHAR(100) NOT NULL, avatar BLOB);', (err) => {
   if (err) throw err;
   console.log('Users table created');
 });
 
-db.query('CREATE TABLE reviews( id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(100) NOT NULL default "Заголовок", workTitle VARCHAR(100) NOT NULL default "Название произведения", description TEXT NOT NULL, cover BLOB, coverTwo BLOB, createDate DATETIME NOT NULL, rating TINYINT NOT NULL, category VARCHAR(100) NOT NULL default "Категория", tags VARCHAR(255) default "Теги", averageReviewRating FLOAT NOT NULL, likes TINYINT, dislikes TINYINT, views TINYINT, comments TEXT,owner VARCHAR(255) NOT NULL)', (err) => {
+db.query('CREATE TABLE categories( id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100) NOT NULL UNIQUE );', (err) => {
+  if (err) throw err;
+  console.log('Categories table created');
+});
+
+db.query('CREATE TABLE reviews( id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(100) NOT NULL default "Заголовок", workTitle VARCHAR(100) NOT NULL default "Название произведения", description TEXT NOT NULL, cover BLOB, coverTwo BLOB, createDate DATETIME NOT NULL, rating TINYINT NOT NULL, user_id INT NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE, category_id INT NOT NULL, FOREIGN KEY (category_id) REFERENCES categories(id) );', (err) => {
   if (err) throw err;
   console.log('Reviews table created');
+});
+
+db.query('CREATE TABLE likes(id INT PRIMARY KEY AUTO_INCREMENT, user_id INT NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id), review_id INT NOT NULL, FOREIGN KEY (review_id) REFERENCES reviews(id) ON DELETE CASCADE, createDate DATETIME NOT NULL);', (err) => {
+  if (err) throw err;
+  console.log('Likes table created');
+});
+
+db.query('CREATE TABLE dislikes(id INT PRIMARY KEY AUTO_INCREMENT, user_id INT NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id), review_id INT NOT NULL, FOREIGN KEY (review_id) REFERENCES reviews(id) ON DELETE CASCADE, createDate DATETIME NOT NULL);', (err) => {
+  if (err) throw err;
+  console.log('Likes table created');
+});
+
+db.query('CREATE TABLE ratings(id INT PRIMARY KEY AUTO_INCREMENT, user_id INT NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE, review_id INT NOT NULL, FOREIGN KEY (review_id) REFERENCES reviews(id) ON DELETE CASCADE, rate TINYINT NOT NULL,createDate DATETIME NOT NULL);', (err) => {
+  if (err) throw err;
+  console.log('Likes table created');
 });
